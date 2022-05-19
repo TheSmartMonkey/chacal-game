c02f04950bc8e4af10cf717e64573f724c088e5a
Laurent VANDELLE <vandellelaurent@yahoo.fr>
2022-05-20 00:46:22 +0200

    init flutter ui

---
+++ ui/lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:ui/screens/home.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    const String title = 'Le jeux du chacal';
+
+    return MaterialApp(
+      title: title,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: title),
+    );
+  }
+}
